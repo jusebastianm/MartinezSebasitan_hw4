2d0152c68d0f4c1aa1fe41e1c0acdf1032cdef49
Juan Sebastian Martinez Acevedo <42219672+jusebastianm@users.noreply.github.com>
2018-11-17 22:14:53 -0500

    Add files via upload

--- ODE.cpp
+++ ODE.cpp
@@ -60,19 +60,167 @@ int main()
 				double ry=y0+h*(k1_posy+2.0*k2_posy+2.0*k3_posy+k4_posy)/6.0;
 				
 				double vel=velocidad(v0, ang, t);
-
-				cout << "El alcance del proyectil a tiempo " << t  << " "<<  "es " << rx << endl;
 				outFile << rx << " " << ry << " " << vel << endl;
 			}	
 
 		outFile.close();
 
 
+		double ang2=10;
+		double ang3=20;
+		double ang4=30;
+		double ang5=40;
+		double ang6=50;
+		double ang7=60;
+		double ang8=70;
 
+		ofstream outFile2; 
+		outFile2.open("proyectil_2.txt");
+		for (int t=0; t<=50; t++)
+			{
+//_______________________________________________________________________________________________________________________________
+//					10 GRADOS 
 
+				double k1_posx_10=pos_x(x0,v0,ang2,t);
+				double k1_posy_10=pos_y(y0,v0,ang2,t);
 
+				double k2_posx_10=pos_x(x0+h*k1_posx_10/2.0,v0,ang2,t+h/2.0);
+				double k2_posy_10=pos_y(y0+h*k1_posy_10/2.0,v0,ang2,t+h/2.0);
+		
+				double k3_posx_10=pos_x(x0+h*k2_posx_10/2.0,v0,ang2,t+h/2.0);
+				double k3_posy_10=pos_y(y0+h*k2_posy_10/2.0,v0,ang2,t+h/2.0);
+	
+				double k4_posx_10=pos_x(x0+h*k3_posx_10,v0,ang2,t+h);
+				double k4_posy_10=pos_y(y0+h*k3_posy_10,v0,ang2,t+h);
 
+				double rx_10=(x0+h*(k1_posx_10+2.0*k2_posx_10+2.0*k3_posx_10+k4_posx_10)/6.0)*-1;
+				double ry_10=(y0+h*(k1_posy_10+2.0*k2_posy_10+2.0*k3_posy_10+k4_posy_10)/6.0)*-1;
+				
+				double vel_10=velocidad(v0, ang2, t);
 
+//________________________________________________________________________________________________________________________________
+//					20 GRADOS 
+
+				double k1_posx_20=pos_x(x0,v0,ang3,t);
+				double k1_posy_20=pos_y(y0,v0,ang3,t);
+
+				double k2_posx_20=pos_x(x0+h*k1_posx_20/2.0,v0,ang3,t+h/2.0);
+				double k2_posy_20=pos_y(y0+h*k1_posy_20/2.0,v0,ang3,t+h/2.0);
+		
+				double k3_posx_20=pos_x(x0+h*k2_posx_20/2.0,v0,ang3,t+h/2.0);
+				double k3_posy_20=pos_y(y0+h*k2_posy_20/2.0,v0,ang3,t+h/2.0);
+	
+				double k4_posx_20=pos_x(x0+h*k3_posx_20,v0,ang3,t+h);
+				double k4_posy_20=pos_y(y0+h*k3_posy_20,v0,ang3,t+h);
+
+				double rx_20=x0+h*(k1_posx_20+2.0*k2_posx_20+2.0*k3_posx_20+k4_posx_20)/6.0;
+				double ry_20=y0+h*(k1_posy_20+2.0*k2_posy_20+2.0*k3_posy_20+k4_posy_20)/6.0;
+				
+				double vel_20=velocidad(v0, ang3, t);
+
+				
+//________________________________________________________________________________________________________________________________
+//					30 GRADOS 
+
+				double k1_posx_30=pos_x(x0,v0,ang4,t);
+				double k1_posy_30=pos_y(y0,v0,ang4,t);
+
+				double k2_posx_30=pos_x(x0+h*k1_posx_30/2.0,v0,ang4,t+h/2.0);
+				double k2_posy_30=pos_y(y0+h*k1_posy_30/2.0,v0,ang4,t+h/2.0);
+		
+				double k3_posx_30=pos_x(x0+h*k2_posx_30/2.0,v0,ang4,t+h/2.0);
+				double k3_posy_30=pos_y(y0+h*k2_posy_30/2.0,v0,ang4,t+h/2.0);
+	
+				double k4_posx_30=pos_x(x0+h*k3_posx_30,v0,ang4,t+h);
+				double k4_posy_30=pos_y(y0+h*k3_posy_30,v0,ang4,t+h);
+
+				double rx_30=x0+h*(k1_posx_30+2.0*k2_posx_30+2.0*k3_posx_30+k4_posx_30)/6.0;
+				double ry_30=y0+h*(k1_posy_30+2.0*k2_posy_30+2.0*k3_posy_30+k4_posy_30)/6.0;
+				
+				double vel_30=velocidad(v0, ang4, t);
+//________________________________________________________________________________________________________________________________
+//					40 GRADOS 
+
+				double k1_posx_40=pos_x(x0,v0,ang5,t);
+				double k1_posy_40=pos_y(y0,v0,ang5,t);
+
+				double k2_posx_40=pos_x(x0+h*k1_posx_40/2.0,v0,ang5,t+h/2.0);
+				double k2_posy_40=pos_y(y0+h*k1_posy_40/2.0,v0,ang5,t+h/2.0);
+		
+				double k3_posx_40=pos_x(x0+h*k2_posx_40/2.0,v0,ang5,t+h/2.0);
+				double k3_posy_40=pos_y(y0+h*k2_posy_40/2.0,v0,ang5,t+h/2.0);
+	
+				double k4_posx_40=pos_x(x0+h*k3_posx_40,v0,ang5,t+h);
+				double k4_posy_40=pos_y(y0+h*k3_posy_40,v0,ang5,t+h);
+
+				double rx_40=x0+h*(k1_posx_40+2.0*k2_posx_40+2.0*k3_posx_40+k4_posx_40)/6.0;
+				double ry_40=y0+h*(k1_posy_40+2.0*k2_posy_40+2.0*k3_posy_40+k4_posy_40)/6.0;
+				
+				double vel_40=velocidad(v0, ang5, t);
+//________________________________________________________________________________________________________________________________
+//					50 GRADOS 
+
+				double k1_posx_50=pos_x(x0,v0,ang6,t);
+				double k1_posy_50=pos_y(y0,v0,ang6,t);
+
+				double k2_posx_50=pos_x(x0+h*k1_posx_50/2.0,v0,ang6,t+h/2.0);
+				double k2_posy_50=pos_y(y0+h*k1_posy_50/2.0,v0,ang6,t+h/2.0);
+		
+				double k3_posx_50=pos_x(x0+h*k2_posx_50/2.0,v0,ang6,t+h/2.0);
+				double k3_posy_50=pos_y(y0+h*k2_posy_50/2.0,v0,ang6,t+h/2.0);
+	
+				double k4_posx_50=pos_x(x0+h*k3_posx_50,v0,ang6,t+h);
+				double k4_posy_50=pos_y(y0+h*k3_posy_50,v0,ang6,t+h);
+
+				double rx_50=x0+h*(k1_posx_50+2.0*k2_posx_50+2.0*k3_posx_50+k4_posx_50)/6.0;
+				double ry_50=y0+h*(k1_posy_50+2.0*k2_posy_50+2.0*k3_posy_50+k4_posy_50)/6.0;
+				
+				double vel_50=velocidad(v0, ang6, t);
+//________________________________________________________________________________________________________________________________
+//					60 GRADOS 
+
+				double k1_posx_60=pos_x(x0,v0,ang7,t);
+				double k1_posy_60=pos_y(y0,v0,ang7,t);
+
+				double k2_posx_60=pos_x(x0+h*k1_posx_60/2.0,v0,ang7,t+h/2.0);
+				double k2_posy_60=pos_y(y0+h*k1_posy_60/2.0,v0,ang7,t+h/2.0);
+		
+				double k3_posx_60=pos_x(x0+h*k2_posx_60/2.0,v0,ang7,t+h/2.0);
+				double k3_posy_60=pos_y(y0+h*k2_posy_60/2.0,v0,ang7,t+h/2.0);
+	
+				double k4_posx_60=pos_x(x0+h*k3_posx_60,v0,ang7,t+h);
+				double k4_posy_60=pos_y(y0+h*k3_posy_60,v0,ang7,t+h);
+
+				double rx_60=x0+h*(k1_posx_60+2.0*k2_posx_60+2.0*k3_posx_60+k4_posx_60)/6.0;
+				double ry_60=y0+h*(k1_posy_60+2.0*k2_posy_60+2.0*k3_posy_60+k4_posy_60)/6.0;
+				
+				double vel_60=velocidad(v0, ang7, t);
+//________________________________________________________________________________________________________________________________
+//					70 GRADOS 
+
+				double k1_posx_70=pos_x(x0,v0,ang8,t);
+				double k1_posy_70=pos_y(y0,v0,ang8,t);
+
+				double k2_posx_70=pos_x(x0+h*k1_posx_70/2.0,v0,ang8,t+h/2.0);
+				double k2_posy_70=pos_y(y0+h*k1_posy_70/2.0,v0,ang8,t+h/2.0);
+		
+				double k3_posx_70=pos_x(x0+h*k2_posx_70/2.0,v0,ang8,t+h/2.0);
+				double k3_posy_70=pos_y(y0+h*k2_posy_70/2.0,v0,ang8,t+h/2.0);
+	
+				double k4_posx_70=pos_x(x0+h*k3_posx_70,v0,ang8,t+h);
+				double k4_posy_70=pos_y(y0+h*k3_posy_70,v0,ang8,t+h);
+
+				double rx_70=x0+h*(k1_posx_70+2.0*k2_posx_70+2.0*k3_posx_70+k4_posx_70)/6.0;
+				double ry_70=y0+h*(k1_posy_70+2.0*k2_posy_70+2.0*k3_posy_70+k4_posy_70)/6.0;
+				
+				double vel_70=velocidad(v0, ang8, t);
+				
+
+				
+outFile2 << rx_10 << " " << ry_10 << " " << vel_10 << " " << rx_20 << " " << ry_20 << " " << vel_20 << " " << rx_30 << " " << ry_30 << " " << vel_30 << " " << rx_40 << " " << ry_40 << " " << vel_40 << " " << rx_50 << " " << ry_50 << " " << vel_50 << " " << rx_60 << " " << ry_60 << " " << vel_60 << " " << rx_70 << " " << ry_70 << " " << vel_70 << endl;
+			}	
+
+		outFile2.close();
 		return 0;
 	}
 
